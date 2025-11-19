b6868b02bf7ff46b356a41e56e4ad525683e05af
rony karmakar <ronykarmakar437@gmail.com>
2025-11-19 23:58:55 +0530

    Color menu is improved

--- client/src/pages/Notes/components/colorPellete.tsx
+++ client/src/pages/Notes/components/colorPellete.tsx
@@ -5,16 +5,20 @@ type editorColorState = Pick<ReturnType<typeof useMyEditorState>, "activeColor" 
 
 const ColorPellete = ({ editorState, editor }: { editorState: editorColorState, editor: Editor }) => {
     return (
-        <div className="flex gap-2 items-center">
+        <div className="absolute flex flex-col gap-2 items-start py-2 px-6 bg-neutral-100 top-13 right-1 rounded-sm">
 
             {/* Color Palette */}
-            <div className="flex gap-1">
-                {["#ff0000", "#00bcd4", "#4caf50", "#ff9800", "#9c27b0"].map((color) => (
+            <div className='text-xs text-neutral-600 font-bold'>Text Color</div>
+            <div className="grid grid-cols-5 gap-3">
+                {[
+                    "#ff0000", "#00bcd4", "#4caf50", "#ff9800", "#9c27b0", "#e91e63", "#3f51b5", "#795548", "#607d8b", "#000000",
+                ].map((color) => (
                     <button
                         key={color}
                         disabled={!editorState.canSetColor}
                         onClick={() => editor.chain().focus().setColor(color).run()}
-                        className={`w-5 h-5 rounded-full ${editorState.activeColor === color ? "ring-2 ring-black" : ""}`}
+                        className={`w-6 h-6 ${editorState.activeColor === color ? "ring-2 ring-black" : ""
+                            }`}
                         style={{ backgroundColor: color }}
                     />
                 ))}
@@ -24,31 +28,32 @@ const ColorPellete = ({ editorState, editor }: { editorState: editorColorState, 
             <button
                 disabled={!editorState.canUnsetColor}
                 onClick={() => editor.chain().focus().unsetColor().run()}
-                className="px-2 py-1 bg-white hover:bg-gray-100"
+                className="text-xs px-2 py-1"
             >
                 Clear
             </button>
-            <div className="flex gap-1 items-center justify-center">
-                {["#ffff00", "#ffeb3b", "#ffcccb", "#b3e5fc", "#c8e6c9"].map((color) => (
+
+            <div className='text-xs text-neutral-600 font-bold'>Background Color</div>
+            <div className="grid grid-cols-5 gap-3">
+                {[
+                    "#ff0000", "#00bcd4", "#4caf50", "#ff9800", "#9c27b0", "#e91e63", "#3f51b5", "#795548", "#607d8b", "#000000",
+                ].map((color) => (
                     <button
                         key={color}
                         disabled={!editorState.canSetHighlight}
-                        onClick={() =>
-                            editor.chain().focus().setHighlight({ color }).run()
-                        }
-                        className={`w-5 h-5 ${editorState.activeHighlight === color ? "ring-2 ring-black" : ""}`}
+                        onClick={() => editor.chain().focus().setHighlight({ color }).run()}
+                        className={`w-6 h-6  ${editorState.activeHighlight === color ? "ring-2 ring-black" : ""}`}
                         style={{ backgroundColor: color }}
                     />
                 ))}
-
-                <button
-                    disabled={!editorState.canUnsetHighlight}
-                    onClick={() => editor.chain().focus().unsetHighlight().run()}
-                    className="px-2 py-1"
-                >
-                    Clear
-                </button>
             </div>
+            <button
+                disabled={!editorState.canUnsetHighlight}
+                onClick={() => editor.chain().focus().unsetHighlight().run()}
+                className="text-xs px-2 py-1"
+            >
+                Clear
+            </button>
         </div>
     )
 }
